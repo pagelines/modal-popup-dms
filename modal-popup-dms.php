1bbc23dc4d980d19f00e5b945dee0e1739647d3a
Chris Johnston <MrFent@gmail.com>
2014-02-04 23:43:47 -0600

    Update images and plugin description

--- modal-popup-dms.php
+++ modal-popup-dms.php
@@ -4,7 +4,7 @@ Plugin Name: Modal PopUp DMS
 Author: MrFent
 Author URI: http://www.MrFent.com
 Demo: http://modal-popup-dms.MrFent.com
-Description: Easily add a PopUp window to any post/page on your site!
+Description: Display an automatic PopUp window anywhere on your DMS website
 Class Name: ModalPopUpDMS
 Version: 1.0.0
 Filter: component
--- sections/modal-popup-dms/section.php
+++ sections/modal-popup-dms/section.php
@@ -4,7 +4,7 @@ Section: Modal PopUp DMS
 Author: MrFent
 Author URI: http://www.MrFent.com
 Demo: http://modal-popup-dms.MrFent.com
-Description: Easily add a PopUp window to any post/page on your site!
+Description: Display an automatic PopUp window anywhere on your DMS website
 Class Name: ModalPopUpDMS
 Version: 1.0.0
 Filter: component
